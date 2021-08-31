a31d302b147708e6af73a2bc4daf1e215cad23df
hyunsu-lee <hyunsu.lee@navercorp.com>
2021-08-31 16:41:36 +0900

    https://github.com/dlgustn555/next-bnb/issues/3 [API-GW] cors 라이브러리 추가

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const Koa = require('koa')
 const bodyParser = require('koa-bodyparser')
+const cors = require('cors')
+
 const routes = require('./routes')
 const mongodb = require('./mongodb')
 
@@ -9,6 +11,7 @@ const app = new Koa()
 
 app.use(bodyParser())
 app.use(routes.routes())
+app.use(cors())
 
 app.listen(8001, () => {
   console.log('Listening on PORT: 8001')
